d57810448d96a2152506d1012b09bc66e1e4075d
bayush114 <86807301+bayush114@users.noreply.github.com>
2021-07-16 19:24:15 -0500

    Create locations.java

---
+++ locations.java
@@ -0,0 +1,8 @@
+public interface locations{
+	public String locations[] = {"New York", "Los Angles", "San Jose", "Chicago", "Houston", "Phoenix", 
+			"Philadelphia", "San Antonio", "San Diego", "Dallas", "Austin", "Miami", "Fort Worth", "Jacksonville", "Columbus", "Charlotte", 
+			"Indianapolis", "San Franciso", "Seattle", "Denver", "Washington", "Dallas", 
+			"Boston" , "El Paso","Nashville", "Oklahoma City", "Las Vegas", "Detriot", 
+			"Portland", "Memphis", "Louisville", "Milwaukee", "Baltimore", "Albuquerque", "Tucson",  "Meso"};
+	public int numpass[] = new int[36];	
+}
